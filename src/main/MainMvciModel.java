e32ef835d686796185b7bdfcf3fb627ec597eb39
Renn Gilbert <renntg@gmail.com>
2024-04-01 17:55:20 -0600

    Added screen booleans to main menu.

--- src/main/MainMvciModel.java
+++ src/main/MainMvciModel.java
@@ -1,5 +1,16 @@
 package main;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
 public class MainMvciModel {
-    
+    private final BooleanProperty boardScreenVisible = new SimpleBooleanProperty();
+    private final BooleanProperty menuScreenVisible = new SimpleBooleanProperty();
+
+    public BooleanProperty boardScreenVisibleProperty() {
+        return boardScreenVisible;
+    }
+    public BooleanProperty menuScreenVisibleProperty() {
+        return menuScreenVisible;
+    }
 }
